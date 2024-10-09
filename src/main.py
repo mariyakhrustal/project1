6bff069e2d927db759e1275a7d64873e7ed51568
Mariya Khrustaleva <mbotmanova@gmail.com>
2024-10-09 21:57:32 +0500

    add linters

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ import re
 
 
 def clear_names(file_name: str) -> list:
-    """ Функция очистки имен от лишних символов """
+    """Функция очистки имен от лишних символов"""
     new_names_list = list()
-    with open('data/' + file_name, encoding='utf-8') as names_file:
+    with open("data/" + file_name, encoding="utf-8") as names_file:
         names_list = names_file.read().split()
         for name_item in names_list:
             new_name = ""
@@ -17,12 +17,12 @@ def clear_names(file_name: str) -> list:
 
 
 def is_cyrillic(name_item: str) -> bool:
-    """ Проверка на вхождение кириллицы в строку """
-    return bool(re.search('[а-яА-Я]', name_item))
+    """Проверка на вхождение кириллицы в строку"""
+    return bool(re.search("[а-яА-Я]", name_item))
 
 
 def filter_russian_names(names_list: list) -> list:
-    """ Фильтрация имен написанных на русском """
+    """Фильтрация имен написанных на русском"""
     new_names_list = list()
     for name_item in names_list:
         if is_cyrillic(name_item):
@@ -31,7 +31,7 @@ def filter_russian_names(names_list: list) -> list:
 
 
 def filter_english_names(names_list: list) -> list:
-    """ Фильтрация имен написанных на английском """
+    """Фильтрация имен написанных на английском"""
     new_names_list = list()
     for name_item in names_list:
         if not is_cyrillic(name_item):
@@ -40,25 +40,19 @@ def filter_english_names(names_list: list) -> list:
 
 
 def save_to_file(file_name: str, data: str) -> None:
-    """ Сохраняет данные в файл """
-    with open('data/' + file_name, 'w', encoding='utf-8') as names_file:
+    """Сохраняет данные в файл"""
+    with open("data/" + file_name, "w", encoding="utf-8") as names_file:
         names_file.write(data)
 
 
-if __name__ == '__main__':
-    cleared_names = clear_names('names.txt')
+if __name__ == "__main__":
+    cleared_names = clear_names("names.txt")
 
     # for i in cleared_names:
     #     print(i)
 
     filtered_names = filter_russian_names(cleared_names)
-    save_to_file(
-        'russian_names.txt',
-        '\n'.join(filtered_names)
-    )
+    save_to_file("russian_names.txt", "\n".join(filtered_names))
 
     filtered_names = filter_english_names(cleared_names)
-    save_to_file(
-        'english_names.txt',
-        '\n'.join(filtered_names)
-    )
+    save_to_file("english_names.txt", "\n".join(filtered_names))
